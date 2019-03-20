312c25f3141376ddae5ff9985beca10ccf5c6965
Oğuz <kaya0oguz@gmail.com>
2019-03-20 20:03:01 +0300

    Article list sayfasÄindaki veriler veritabanıindan alindi. Article show sayfasi eklendi.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,33 +11,41 @@ use App\Entity\Article;
 class ArticleController extends Controller
 {
   /**
-   * @route("/")
+   * @route("/", name="article_list")
    * @Method({"GET"})
    */
   public function index()
   {
-    // return new Response('<html><body>Hello</body></html>');
+    $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
-    $articles = ['Article 1', 'Article 2'];
-
-    return $this->render('articles/index.html.twig', array('name' => 'Oguz', 'articles' => $articles));
+    return $this->render('articles/index.html.twig', array('articles' => $articles));
   }
 
   /**
-   * @Route("article/save")
+   * @Route("/article/{id}", name="article_show")
    */
-  public function save()
+  public function show($id)
   {
-    $entityManager = $this->getDoctrine()->getManager();
+    $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
-    $article = new Article();
-    $article->setTitle('Article One');
-    $article->setBody('Article One Body');
-
-    $entityManager->persist($article);
-
-    $entityManager->flush();
-
-    return new Response($article->getId() . ' - ' . $article->getTitle() . ', ' . $article->getBody() . 'Makalesi veritabanına eklendi');
+    return $this->render('articles/show.html.twig', array('article' => $article));
   }
+
+  // /**
+  //  * @Route("article/save")
+  //  */
+  // public function save()
+  // {
+  //   $entityManager = $this->getDoctrine()->getManager();
+
+  //   $article = new Article();
+  //   $article->setTitle('Article One');
+  //   $article->setBody('Article One Body');
+
+  //   $entityManager->persist($article);
+
+  //   $entityManager->flush();
+
+  //   return new Response($article->getId() . ' - ' . $article->getTitle() . ', ' . $article->getBody() . 'Makalesi veritabanına eklendi');
+  // }
 }
