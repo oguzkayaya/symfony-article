043899f95aaa1a3492a819620429e864a3596236
Oğuz <kaya0oguz@gmail.com>
2019-03-23 12:04:22 +0300

    new article ekleme sayfaÄsı yapıldı

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Article;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleController extends Controller
 {
@@ -21,6 +25,38 @@ class ArticleController extends Controller
     return $this->render('articles/index.html.twig', array('articles' => $articles));
   }
 
+
+  /**
+   * @Route("/article/new", name="new_article")
+   * @Method({"GET", "POST"})
+   */
+  public function new(Request $request)
+  {
+    $article = new Article();
+    $form = $this->createFormBuilder($article)
+      ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+      ->add('body', TextareaType::class, array(
+        'required' => false, 'attr' => array('class' => 'form-control')
+      ))
+      ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
+      ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $article = $form->getData();
+
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($article);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('article_list');
+    }
+
+
+    return $this->render('articles/new.html.twig', array('form' => $form->createView()));
+  }
+
   /**
    * @Route("/article/{id}", name="article_show")
    */
@@ -30,6 +66,8 @@ class ArticleController extends Controller
 
     return $this->render('articles/show.html.twig', array('article' => $article));
   }
+
+
 
   // /**
   //  * @Route("article/save")
